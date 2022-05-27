66779b3cef6ecf2e5c89a177349ecc3abdd33d66
CDMengual <cdmengual@gmail.com>
2022-05-27 15:54:54 -0300

    conexion con angular

--- src/main/java/com/portfolio/Cesar/Controller/ControllerUsuario.java
+++ src/main/java/com/portfolio/Cesar/Controller/ControllerUsuario.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.portfolio.Cesar.service.IUsuarioService;
 
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://portfolio-cdmengual.web.app"})
 @RestController
 public class ControllerUsuario {
     
